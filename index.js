dcdf89d2f10b96af2e2ce33f01499b4784bdab61
Leandroti7 <leandro.dasilvati7@gmail.com>
2019-05-22 16:01:44 -0300

    finishing curse starting

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
     const express = require('express');
     const bodyParser = require('body-parser');
+    const watson = require('ibm-watson/assistant/v1');
+
+    const  watsonAuth = require('./watsonAuth');
 
     const port = 8000;
-
     const app = express();
 
     app.use(bodyParser.json());
+    app.use(express.static(__dirname + '/public'));
+    app.use(express.static(__dirname + '/resources'));
+
+    const assistant = new  watson({
+        username: watsonAuth.username,
+        password: watsonAuth.password,
+        url: 'https://gateway.watsonplatform.net/assistant/api/',
+        version: '2019-02-28'
+    });
+
+    app.get('/', function (req, res) {
+        return res.sendFile('./public/index.html');
+    });
+    app.get('/', function (req, res) {
+        return res.sendFile('./resources/style.css');
+    });
 
     app.post('/dialog', (req, res) => {
         const { message } = req.body;
-
-        res.json({ leandro: message });
-
+        // console.log(message);
+        assistant.message(
+            {
+                input: { text: message },
+                workspace_id: watsonAuth.workspace_id
+            },
+            function (err, response) {
+                // console.log();
+                if (err) {
+                    console.error(err);
+                } else{
+                    const { output } = response;
+                    return res.json(output.text);
+                }
+            }
+        );
     });
 
     app.listen(port,() => console.log(`Running on port ${port}`));
---
+++ watsonAuth.js
@@ -0,0 +1,5 @@
+module.exports = {
+    username: 'apikey',
+    password: 'MJ_fkVhAIcCTt-wWP-V8uvhkGcIkd8YMWShtEx9Ggvsc',
+    workspace_id: '6969f3ba-ffc0-4084-bb87-359d52f4ab62'
+}
